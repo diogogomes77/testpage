5aace120da643d050fb47d305ec32301d10158e2
Diogo Gomes <diogo.gomes77@gmail.com>
2022-04-13 16:14:40 +0100

    feat: add lint and yarn

--- pages/index.js
+++ pages/index.js
@@ -2,7 +2,7 @@ import Link from 'next/link';
 export default function Home() {
   return (
     <div>
-      Hello World test1.{' '}
+      Hello World lint yarn.{' '}
       <Link href='/about' as={process.env.BACKEND_URL + '/about'}>
         <a>About</a>
       </Link>
